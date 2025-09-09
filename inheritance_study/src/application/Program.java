a12e7e1ca2c0f91e868aac33121c405bcf248631
João Lucas <jlucasbs1@gmail.com>
2025-09-09 10:26:56 -0300

    feat: add study examples on inheritance, upcasting, downcasting and polymorphism

---
+++ inheritance_study/src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Account acc = new Account(1001, "Alex", 1000.0);
+		
+		
+		// UPCASTING
+		Account acc2 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		Account acc3 = new SavingsAccount(1004, "Anna",1000.0, 0.01);
+		
+		
+		// DOWNCASTING
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+//			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+//			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+			
+		acc.withdraw(200.00);
+		System.out.println(acc.getBalance());
+		
+		acc3.withdraw(200.00);
+		System.out.println(acc3.getBalance());
+		
+		Account acc6 = new BusinessAccount(1005, "Anna",1000.0, 500.0);
+		
+		acc6.withdraw(200);
+		System.out.println(acc6.getBalance());
+	}
+
+}

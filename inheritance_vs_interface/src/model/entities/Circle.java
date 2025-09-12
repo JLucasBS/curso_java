0ed02932e134b88a2960f4cac1c0ad3a28ca4055
João Lucas <jlucasbs1@gmail.com>
2025-09-12 09:19:44 -0300

    feat: implement abstract shape and inheritance shape

---
+++ inheritance_vs_interface/src/model/entities/Circle.java
@@ -0,0 +1,32 @@
+package model.entities;
+
+import model.enums.Color;
+
+public class Circle extends AbstractShape {
+
+	private Double radius;
+
+	public Circle(Color color, Double radius) {
+		super(color);
+		this.radius = radius;
+	}
+
+	public Double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(Double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [area=" + String.format("%.3f", area()) + ", Color=" + getColor() + "]";
+	}
+
+}

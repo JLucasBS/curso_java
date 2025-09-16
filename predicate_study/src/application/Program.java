ae833ec2396b80b832333c545735049728de62be
João Lucas <jlucasbs1@gmail.com>
2025-09-16 16:54:12 -0300

    feat: study predicate with lambda

---
+++ predicate_study/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Predicate;
+
+import model.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+//		list.removeIf(new ProductPredicate()); -> implemetação da interface
+//		list.removeIf(Product::staticProductPredicate); -> reference method com metodo estatico
+//		list.removeIf(Product::nonStaticProductPredicate); -> reference method com metodo não estatico
+
+		double min = 100.0;
+		
+//		Predicate<Product> pred = p -> p.getPrice() >= min; -> expressão lambda declarada
+//		list.removeIf(pred);
+		
+		list.removeIf(p -> p.getPrice() >= 100);
+		
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+
+}

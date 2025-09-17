79448b3ef60480d5da3aa009040858a5480bb0b9
João Lucas <jlucasbs1@gmail.com>
2025-09-17 11:02:38 -0300

    feat: implementing stream

---
+++ stream_study/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+
+		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);
+		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+
+		Stream<Long> fibonacci = Stream.iterate(new Long[] {0L, 1L}, x -> new Long[] {x[1], x[0] + x[1]})
+				.map(x -> x[0]);
+		System.out.println(Arrays.toString(fibonacci.limit(10).toArray()));
+	}
+
+}
